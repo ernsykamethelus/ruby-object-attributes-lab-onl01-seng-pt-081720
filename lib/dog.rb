771bdac49ebb08fef5bfb810d7ab947ee7f9ab9c
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-01 03:28:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,20 @@
 class Dog
   
-  def initialize(name)
+  def name(name)
     @name=name
   end
   
   def name
     @name
   end
+end
+
+def breed=(breed)
+  @breed=breed
+end
+
+def breed
+  @breed
 end
   
   fido= Dog.new("fido")
